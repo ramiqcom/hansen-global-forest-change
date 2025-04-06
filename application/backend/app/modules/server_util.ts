070fe0f4b7d238653eefce969c129ae418b9c9f3
Ramadhan <ramiqcom@gmail.com>
2025-04-06 20:44:26 +0200

    use exec

--- application/backend/app/modules/server_util.ts
+++ application/backend/app/modules/server_util.ts
@@ -1,31 +1,20 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 export async function execute_process(cmd: string, args: any[]): Promise<number> {
   return await new Promise((resolve, reject) => {
-    const process = spawn(cmd, args);
-    const output = [];
-    const error = [];
-
-    process.stdout.on('data', (data) => {
-      output.push(data);
-    });
-
-    process.stderr.on('data', (data) => {
-      error.push(data);
-    });
-
-    process.on('close', (code) => {
-      if (code === 0) {
-        console.log(output.join('\n'));
-        resolve(0);
-      } else {
-        const message = error.join('\n');
-        console.error(message);
-        reject(new Error(message));
+    exec(`${cmd} ${args.join(' ')}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
       }
-    });
 
-    process.on('error', (error) => {
-      reject(error);
+      if (stderr) {
+        console.error(stderr);
+      }
+
+      if (stdout) {
+        console.log(stdout);
+      }
+
+      resolve(0);
     });
   });
 }
